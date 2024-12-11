cd06a59583c112b142d2d73d65243e156c792c9c
101045330 <101045330@georgebrown.ca>
2024-12-11 03:43:43 +0000

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 //import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
